2c0b30475c36618a61bf3f45a09713f4ca540732
Jenna-Zing <47491259+Jenna-Zing@users.noreply.github.com>
2025-12-05 13:22:47 -0600

    added CORS middleware

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express"); // load the express library
+const cors = require("cors"); // loads cors library - cross-origin resource sharing -> if FE and BE run on different ports, the browser considers them different "origins", so the browser will block the requests.
+// with CORS, you allow your frontend (FE) to talk to your backend (BE) -> thus, your client (FE) can call the API endpoints in your server.  See the associated route for the BE code that handles that endpoint address.
+
 const app = express(); // creates an express app (backend program), so I can define routes, middlewares, etc.
 
 // adding middleware (code that runs *before* your route handler) to parse JSON in request bodies -> raw text gets converting to JS object and put into `req.body`
 app.use(express.json());
+
+// adding CORS middleware so the frontend can talk to the backend
+app.use(cors()); // browser security policy, allowing frontend -> backend requests
 
 // A basic route for sample API endpoint (just logging route works)
 app.get("/", (req, res) => {
